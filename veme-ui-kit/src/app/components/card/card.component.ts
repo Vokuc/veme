a6631f4d33da9d8e33fa8af60e30c4f92429d998
Chika Ukpai <chikau@phreetech.com>
2025-02-12 23:14:27 +0100

    added more customization to the card component

--- veme-ui-kit/src/app/components/card/card.component.ts
+++ veme-ui-kit/src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgClass],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
@@ -14,6 +15,6 @@ export class CardComponent {
   @Input() imageUrl: string = 'https://via.placeholder.com/150';
   @Input() buttonText: string = 'Learn More';
   @Input() buttonType: string = 'primary';
-
-
+  @Input() cardClass: string = '';
+  @Output() buttonClick = new EventEmitter<void>();
 }
